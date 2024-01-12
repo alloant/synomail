378064b349354be92d077d95c709addcaf9c1061
Antonio <alloant@gmail.com>
2024-01-12 13:38:53 +0530

    Cleaning

--- run_mariadb_local.sh
+++
@@ -1,3 +0,0 @@
-#DATABASE_URI="mariadb+pymysql://Aes:sG1zDL%=W]W(l-d\$|-yxzVEJ@nas.prome.sg/synomail?charset=utf8mb4" poetry run waitress-serve --threads=6 --host=100.97.32.113 --port=8041 --call 'app:create_app'
-DATABASE_URI="mariadb+pymysql://root:]9sT3vn+&R(a$]bxD\"hWP^eHb@localhost/synomail?charset=utf8mb4" poetry run waitress-serve --threads=6 --host=100.97.32.113 --port=8041 --call 'app:create_app'
-
--- start_mariadb_local.sh
+++
@@ -1,4 +0,0 @@
-#DATABASE_URI="mariadb+pymysql://root:]9sT3vn+&R(a$]bxD\"hWP^eHb@localhost/synomail?charset=utf8mb4" poetry run flask db init
-DATABASE_URI="mariadb+pymysql://root:]9sT3vn+&R(a$]bxD\"hWP^eHb@localhost/synomail?charset=utf8mb4" poetry run flask db migrate -m "Init"
-DATABASE_URI="mariadb+pymysql://root:]9sT3vn+&R(a$]bxD\"hWP^eHb@localhost/synomail?charset=utf8mb4" poetry run flask db upgrade
-
